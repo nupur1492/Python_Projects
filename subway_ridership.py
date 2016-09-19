f0903204fcf93ec6f1994bdab3e9b4df0c47bebe
nupur1492 <nupur.gulalkari@gmail.com>
2016-09-19 18:59:15 -0400

    Modified Regression technique

--- subway_ridership.py
+++ subway_ridership.py
@@ -6,7 +6,7 @@ from sklearn import cross_validation
 from sklearn.metrics import accuracy_score
 import numpy as np
 import matplotlib.pyplot as plt
-
+import statsmodels
 
 data = pd.read_csv("turnstile_data_master_with_weather.csv")
 df_subway = pd.DataFrame(data)
@@ -26,19 +26,48 @@ ttest = scipy.stats.ttest_ind(df_subway_rain.ENTRIESn_hourly, df_subway_norain.E
 
 print("t-test:  ", ttest)
 
-
-
-if ttest[1] > 0.05:
-    print("Significant p value, Null hypothesis rejected")
+if ttest[1] < 0.05:
+    print("Small p value, Null hypothesis rejected")
 
 # Add day of the week to dataframe
 df_subway.DATEn = pd.to_datetime(df_subway.DATEn)
 df_subway['dayofweek'] = df_subway['DATEn'].apply(lambda x: x.strftime('%w'))
 
 
+p1 = ggplot(aes(x = 'maxpressurei', y = 'ENTRIESn_hourly'), data = df_subway) + geom_point()
+#print(p1)
+
+p2 = ggplot(aes(x = 'maxtempi', y = 'ENTRIESn_hourly'), data = df_subway) + geom_point()
+#print(p2)
+
+p3 = ggplot(aes(x = 'meanwindspdi', y = 'ENTRIESn_hourly'), data = df_subway) + geom_point()
+print(p3)
+
+# p1, p2, p3 show normal distribution
+
 # split dataframe into training and testing sets
 
 train, test = cross_validation.train_test_split(df_subway, test_size=0.3)
+
+
+#Regression:
+import statsmodels.formula.api as sma
+lm = sma.ols(formula = 'ENTRIESn_hourly ~ maxtempi + maxpressurei + meanwindspdi + Hour', data = train).fit()
+print("Regression intercepts and coefficients", lm.params)
+
+print(lm.summary(), lm.conf_int(), lm.pvalues)
+
+# p < 0.05 will denote a relationship between features and output
+
+
+test_columns = ['maxtempi', 'maxpressurei', 'meanwindspdi', 'Hour']
+test = pd.DataFrame(test, columns=test_columns)
+test.head()
+
+#Predict using test set
+pred = lm.predict(test)
+
+
 
 # Specific features/input columns for training
 feature_columns = ['Hour', 'maxpressurei', 'maxdewpti', 'maxtempi', 'meanwindspdi', 'rain', 'fog', 'precipi', 'thunder', 'dayofweek' ]
@@ -53,30 +82,6 @@ labels_test = np.array(test['ENTRIESn_hourly'])
 #labels_test = test['ENTRIESn_hourly']
 
 print(len(features_train), len(labels_train), len(features_test), len(labels_test))
-
-
-#Machine Learning
-# 1. Multiple Linear Regression
-from sklearn import linear_model
-
-reg_mlr = linear_model.LinearRegression()
-reg_mlr.fit(features_train,labels_train)
-
-reg_pred = reg_mlr.predict(features_test)
-print(reg_pred[1:10])
-print(labels_test[1:10])
-
-#print('Multiple Linear Regression Score:  ', reg_mlr.score(features_test, labels_test))
-#print('Multiple Linear Regression Score:  ', reg_mlr.score(labels_test, reg_pred))
-print('Residual Sum of Squares: ', np.mean(abs(reg_pred - labels_test)))
-
-# Plot Regression:
-plt.figure()
-#plt.scatter(features_test, labels_test, color = 'black')
-plt.plot(features_test, reg_pred, color = 'blue')
-
-plt.show()
-
 
 # Decision Trees
 from sklearn import tree
